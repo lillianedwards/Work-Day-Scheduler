f009ad75634065b17761cb1b15a8b22a3870db86
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-09-03 12:03:23 -0400

    Added code to change classes of workday blocks with for loop , created local storage variables and function

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Work-Day-Scheduler
+
+## Description 
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,24 @@
-//VARIABLE DECLARATIONS
+// wrap all the code that interacts with the DOM in a call to jQuery to ensure 
+//that the code isn't run until the browser is finished rendering all the 
+//elements in the html.
 
+$(function () {
+//VARIABLE DECLARATION
+
+//reference to where the time is being displayed in the header
 var timeDisplay = $("#currentDay");
+// declare a variable to use dayjs() function to tell time 
 var today = dayjs();
-timeDisplay.text(today.format("dddd, MMMM, D, h:mmA"));
 
-var saveEventBtn = $("#save-event");
+//H is setting the hour in military time, parseInt takes a string and returns as a number for comparing against div hour times
+var currentHour = parseInt(today.format("H"));
 
-var currentHour = moment().format("hA");
+//storing a reference to the saveBtn class that was [
+var saveEventBtn = $(".saveBtn");
 
-
-var hours = ["9am", "10am", "11am", "12pm" ""]
-
-var hour9 = $("#hour-9");
-var hour10 = $("#hour-10");
-var hour11 = $("#hour-11");
-
-
-
-$("hourDivs") = []
-
-//FUNCTIONS
-
-var setTimeClassSaveEvents = function () {
-  for (let i = 9; i < length; i++) {
-    const element = arr];
-    
-  }
-}
-// $(function storeEvents() {
-  //   console.log("I am in the function")
-  //  });
+// Declaring global variable called storedEvents to keep locally stored events in, JSON parse is turning the string into an object. ? is if ____________ : else return an empty object 
+var storedEvents = localStorage.getItem("events") ? JSON.parse(localStorage.getItem("events")) : {};
   
-  
-  
-  
-  
-  
-  
-  //EVENT LISTENERS
-  $(".saveBtn").on("click", setTimeClassSaveEvents)
   
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -46,50 +26,66 @@ var setTimeClassSaveEvents = function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  
+  //event listener for clicking save button in each time div
+  $(".saveBtn").on("click", SaveEvents) 
+  
   //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+
+
+
+
+
+
+
+
+
+
+
+
+  //Function Purpose: Store Events in Local Storage
+  
+  var SaveEvents = function (event) {
+    // for (let i = 0; i < length; i++) {// how do i turn this on to save all of them 
+    
+    //targeting a sibling of the save button[1] in the array of the div, this targets that div and value is getting the value of the text area
+    var textArea = $(this).siblings()[1].value;
+    
+    //the div is the parent of the submit button and by using this we are targeting the div of the button clicked, and the div is position 0 in the array of that element, we don't want the value, we want the id. 
+    var hourId = $(this).parent()[0].id;
+    
+    storedEvents [hourId] = textArea;
+    //Set the two variable in local storage 
+    localStorage.setItem("events", JSON.stringify({hourId, textArea}))
+    
+  };
+  //Set Dat and Time Display with desired format on CurrentDay id
+  timeDisplay.text(today.format("dddd, MMMM, D, YYYY h:mm A"));
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  
 
-
-
-
-
-
-
-
-
-
-
-
-
-  var event9 = $("#event-9");
-  var event10 = $("#event-10");
-  var event11 = $("#event-11");
-  var event12 = $("#event-12");
-  var event13 = $("#event-13");
-  var event14 = $("#event-14");
-  var event15 = $("#event-15");
-  var event16 = $("#event-16");
-  var event17 = $("#event-17");
-  
-  var eventsAllDay = {
-    Events9AM: event9.val(),
-    Events10AM: event10.val(),
-    Events11AM: event11.val(),
-    Events12PM: event12.val(),
-    Events1PM: event13.val(),
-    Events2PM: event14.val(),
-    Events3PM: event15.val(),
-    Events4PM: event16.val(),
-    Events5PM: event17.val(),
+  //the for loop uses military time and we want to move through hours 9 to 5 (18 in Military)
+  for (var i = 9 ;i < 18; i++) {
+    //setting a variable of time to be the hour- id plus the number of 9 - 17 so we can loop through all the hour divs without making an array of all of the id's
+    var time = $("hour-" + i);
+    //if the current hour matches the hour div, give this div the present styling
+    if (i === currentHour) {
+      time.addClass("present");
+      //else if the current hour is less than the hour div, give it future styling
+    } else if (i > currentHour) {
+      time.addClass("future")
+    } else {
+      //anything else gie it past styling 
+      time.addClass("past")
+    }
   };
+  
+});
+
