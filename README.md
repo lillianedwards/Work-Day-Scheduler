ca8dfea8de423830d13d51c709e454608cd586bb
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-09-04 15:13:33 -0500

    Fixed local storage key/value assignment, fixed for loop class assignment, finished README

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 
 ## Description 
 
+Busy workdays are a common experience for employees across industries. This daily scheduler is a great tool to help employees keep their tasks and obligations organized. 
+
+The Workday Scheduler Features:
+
+📅 Time blocks that change color based on the time of day.
+
+⬜️ Gray: Past
+🟥 Red: Gray
+🟩 Green: Future 
+
+📅 Events saved in local storage that remain on the scheduler even after a page refresh. 
+_________________________________________
+
+## Access 🔗
+
+To access the workday scheduler, simply click on the GitHub Pages link in the repo description or follow this link: [Workday Scheduler](lillian.edwards.gihub.io/Work-Day-Scheduler) 
+_________________________________________
+
+## Usage 
+![Workday Scheduler Landing Page](./assets/images/Scheduler1.png)
+![Workday Scheduler Landing Page](./assets/images/Scheduler2.png)
+
+
+## Credit 
+I worked on this project alone, but received help from my TA Justin Moore and classmate, Pete Richards.
+
+
+## License 
+This project is not licensed. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -28,40 +28,29 @@ var storedEvents = localStorage.getItem("events") ? JSON.parse(localStorage.getI
   // useful when saving the description in local storage?
   
   //event listener for clicking save button in each time div
-  $(".saveBtn").on("click", SaveEvents) 
   
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-
-
-
-
-
-
-
-
-
-
-
-
+  
   //Function Purpose: Store Events in Local Storage
   
   var SaveEvents = function (event) {
     // for (let i = 0; i < length; i++) {// how do i turn this on to save all of them 
-    
     //targeting a sibling of the save button[1] in the array of the div, this targets that div and value is getting the value of the text area
     var textArea = $(this).siblings()[1].value;
     
     //the div is the parent of the submit button and by using this we are targeting the div of the button clicked, and the div is position 0 in the array of that element, we don't want the value, we want the id. 
     var hourId = $(this).parent()[0].id;
     
-    storedEvents [hourId] = textArea;
+    storedEvents[hourId] = textArea;
     //Set the two variable in local storage 
-    localStorage.setItem("events", JSON.stringify({hourId, textArea}))
+    localStorage.setItem("events", JSON.stringify(storedEvents))
     
   };
+
+ saveEventBtn.on("click", SaveEvents);
   //Set Dat and Time Display with desired format on CurrentDay id
   timeDisplay.text(today.format("dddd, MMMM, D, YYYY h:mm A"));
   
@@ -74,7 +63,7 @@ var storedEvents = localStorage.getItem("events") ? JSON.parse(localStorage.getI
   //the for loop uses military time and we want to move through hours 9 to 5 (18 in Military)
   for (var i = 9 ;i < 18; i++) {
     //setting a variable of time to be the hour- id plus the number of 9 - 17 so we can loop through all the hour divs without making an array of all of the id's
-    var time = $("hour-" + i);
+    var time = $("#hour-" + i);
     //if the current hour matches the hour div, give this div the present styling
     if (i === currentHour) {
       time.addClass("present");
